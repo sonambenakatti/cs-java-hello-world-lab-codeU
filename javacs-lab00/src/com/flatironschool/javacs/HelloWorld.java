1664e0ced41c90a5c427b78ff355312221920f58
Sonam <sonam.benakatti@gmail.com>
2016-03-24 22:43:12 -0500

    commit

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,22 @@
 package com.flatironschool.javacs;
 
-public class HelloWorld {
-    public static Double getVersion() {
-        // I won't work!
-    }
 
-    public static void main(String[] args) {
-	    // you can test the output of getVersion() here
-    }
+public class HelloWorld {
+    
+	public static Double getVersion() {
+     
+		String s = System.getProperty("java.version");
+		s = s.substring(0,3);
+		return Double.parseDouble(s);
+	}
+
+    
+	public static void main(String[] args) {
+	    
+		// you can test the output of getVersion() here
+    
+  
+	}
+
 }
+
